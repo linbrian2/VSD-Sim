514faf98582a528ed093975e65dd1fe28e7973cc
Brian Lin <brian.lin@i-a-i.com>
2023-05-30 10:51:53 -0400

    Removed unnecessary files from backend

--- backend/src/main/groovy/com/iai/traffic/repository/anomaly/BluetoothAnomalyDeviceRepository.groovy
+++
@@ -1,9 +0,0 @@
-package com.iai.traffic.repository.anomaly
-
-import com.iai.traffic.domain.anomaly.BluetoothAnomalyDevice
-import io.micronaut.data.annotation.Repository
-import io.micronaut.data.repository.CrudRepository
-
-@Repository(value = 'anomaly')
-interface BluetoothAnomalyDeviceRepository extends CrudRepository<BluetoothAnomalyDevice, Long> {
-}
